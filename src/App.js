50b44cfec20e43b5b3b9fee73fc980c81ccb0b16
Patrick Basco <patrickmbasco@gmail.com>
2020-03-04 12:30:52 -0800

    put-together

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard  lastName="Doe" firstName= "Jane" age="45" hairColor="Black"/>
-      <PersonCard  lastName="Smith" firstName="John" age="88" hairColor="Brown"/>
-      <PersonCard  lastName="Fillmore" firstName="Millard" age="50" hairColor="Brown"/>
-      <PersonCard  lastName="Smith" firstName="Maria" age="62" hairColor="Black"/>
+      <PersonCard  lastName="Doe" firstName= "Jane" age={45} hairColor="Black"/>
+      <PersonCard  lastName="Smith" firstName="John" age={88} hairColor="Brown"/>
+      <PersonCard  lastName="Fillmore" firstName="Millard" age={50} hairColor="Brown"/>
+      <PersonCard  lastName="Smith" firstName="Maria" age={62} hairColor="Black"/>
 
 
 
